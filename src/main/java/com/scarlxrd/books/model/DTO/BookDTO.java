8c6536362920a7fa57f42f46874af8d0848b5a12
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-07-26 19:06:08 -0300

    feat: Old code where it was better to start over than to refactor the whole thing.

--- src/main/java/com/scarlxrd/books/model/DTO/BookDTO.java
+++
@@ -1,6 +0,0 @@
-package com.scarlxrd.books.model.DTO;
-
-import com.scarlxrd.books.model.entity.Client;
-
-public record BookDTO(String name, String description, String gender, String year, Long id, Client client) {
-}
--- src/main/java/com/scarlxrd/books/model/DTO/ClientDTO.java
+++
@@ -1,9 +0,0 @@
-package com.scarlxrd.books.model.DTO;
-
-import com.scarlxrd.books.model.entity.Book;
-
-import java.util.List;
-
-public record ClientDTO(String name, Integer age, List<Book> books, Long id, Integer password) {
-
-}
