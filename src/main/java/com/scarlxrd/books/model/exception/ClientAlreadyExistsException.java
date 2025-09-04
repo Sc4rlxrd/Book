23620470ee880336b8109b730aea92af98e08e75
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-09-03 22:12:06 -0300

    feat: creating custom exceptions.

---
+++ src/main/java/com/scarlxrd/books/model/exception/ClientAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.scarlxrd.books.model.exception;
+
+public class ClientAlreadyExistsException extends RuntimeException{
+    public ClientAlreadyExistsException(String message) {
+        super(message);
+    }
+}
