e3f673df43d6ad67ff82942f1fcf3e635989ca66
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-09-07 18:04:54 -0300

    feat: creating the userRepository.

---
+++ src/main/java/com/scarlxrd/books/model/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.scarlxrd.books.model.repository;
+
+import com.scarlxrd.books.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByEmail(String email);
+}
