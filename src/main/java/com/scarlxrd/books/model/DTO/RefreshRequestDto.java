c7cc2962a64b372a7319a95a08c83b94defeacab
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-09-27 19:22:23 -0300

    Revert "feat: feat: add refresh token with redis."
    This reverts commit 3f71528c9b9e71a3750400e82448a0cc71011081.

--- src/main/java/com/scarlxrd/books/model/DTO/RefreshRequestDto.java
+++
@@ -1,4 +0,0 @@
-package com.scarlxrd.books.model.DTO;
-
-public record RefreshRequestDto(String refreshToken) {
-}
--- src/main/java/com/scarlxrd/books/model/DTO/TokenResponseDto.java
+++
@@ -1,4 +0,0 @@
-package com.scarlxrd.books.model.DTO;
-
-public record TokenResponseDto(String accessToken, String refreshToken) {
-}
