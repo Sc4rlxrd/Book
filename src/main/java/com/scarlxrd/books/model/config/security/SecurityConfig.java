5fe316b7578ed2df72149022d6e98adb7c07dd2e
Guilherme dos Santos <guilhermesantos2004gss@gmail.com>
2025-10-11 13:16:31 -0300

    fix: refresh token logic was wrong.

--- src/main/java/com/scarlxrd/books/model/config/security/SecurityConfig.java
+++ src/main/java/com/scarlxrd/books/model/config/security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/logout").authenticated()
-                        .requestMatchers(HttpMethod.POST, "/auth/refresh").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/auth/refresh").permitAll()
                         .requestMatchers(HttpMethod.DELETE,"/v1/clients/{cpf}").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .httpBasic(withDefaults())
