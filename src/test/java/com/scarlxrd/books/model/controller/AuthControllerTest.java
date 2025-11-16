7c6096ab0472dd2206c303d4cac2a3768ce69140
Guilherme D. Santos <guilhermesantos2004gss@gmail.com>
2025-11-16 16:51:28 -0300

    test(auth): Implements tests for login and register endpoints.

--- src/test/java/com/scarlxrd/books/model/controller/AuthControllerTest.java
+++ src/test/java/com/scarlxrd/books/model/controller/AuthControllerTest.java
@@ -2,7 +2,9 @@ package com.scarlxrd.books.model.controller;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.scarlxrd.books.model.DTO.AuthenticationDTO;
 import com.scarlxrd.books.model.DTO.RefreshRequestDto;
+import com.scarlxrd.books.model.DTO.RegisterDTO;
 import com.scarlxrd.books.model.config.redis.RedisService;
 import com.scarlxrd.books.model.config.security.TokenService;
 import com.scarlxrd.books.model.entity.Role;
@@ -16,6 +18,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -153,5 +157,83 @@ public class AuthControllerTest {
         mockMvc.perform(post("/auth/logout").header("Authorization", "InvalidToken")).andExpect(status().isOk());
         verify(redisService, never()).blackListToken(anyString(),anyLong());
 
+    }
+
+    @Test
+    @DisplayName("Should return 200 and tokens on successful authentication.")
+    void login_Success_ReturnsTokens() throws Exception{
+
+        // Arrange
+        AuthenticationDTO dto = new AuthenticationDTO("user@user.com","plainPassword");
+        User mockUser = new User("user@user.com","encrypted-pass",Set.of(Role.USER));
+        var authentication = mock(Authentication.class);
+
+        when(authentication.getPrincipal()).thenReturn(mockUser);
+        when(authenticationManager.authenticate(any())).thenReturn(authentication);
+
+        String accessToken = "access.token";
+        String refreshToken = "refresh.token";
+        String jti = "some-jti";
+
+        when(tokenService.generateAccessToken(mockUser)).thenReturn(accessToken);
+        when(tokenService.generateRefreshToken(mockUser)).thenReturn(refreshToken);
+        when(tokenService.getJti(refreshToken)).thenReturn(jti);
+        when(tokenService.getExpiration(refreshToken)).thenReturn(Instant.now().plusSeconds(3600));
+
+        // Act e Assert
+        mockMvc.perform(post("/auth/login").contentType(String.valueOf(MediaType.APPLICATION_JSON)).content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").value(accessToken))
+                .andExpect(jsonPath("$.refreshToken").value(refreshToken));
+
+        verify(authenticationManager, times(1)).authenticate(any());
+        verify(tokenService,times(1)).generateAccessToken(mockUser);
+        verify(tokenService,times(1)).generateRefreshToken(mockUser);
+        verify(redisService, times(1)).saveRefreshToken(eq(jti),anyLong());
+    }
+
+    @Test
+    @DisplayName("Should return 401 when credentials are invalid")
+    void login_InvalidCredentials_ReturnsUnauthorized() throws Exception{
+
+        AuthenticationDTO dto = new AuthenticationDTO("user@user.com", "wrong");
+        when(authenticationManager.authenticate(any())).thenThrow(new BadCredentialsException("bad"));
+
+        // Act e Assert
+        mockMvc.perform(post("/auth/login").contentType(String.valueOf(MediaType.APPLICATION_JSON)).content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("Should create user and return 200")
+    void register_Success() throws Exception{
+        RegisterDTO dto = new RegisterDTO("new@user.com","strongPassword",Set.of(Role.USER));
+
+        when(userRepository.findByEmail(dto.email())).thenReturn(null);
+
+        mockMvc.perform(post("/auth/register").contentType(String.valueOf(MediaType.APPLICATION_JSON)).content(objectMapper.writeValueAsString(dto))).andExpect(status().isOk());
+
+        verify(userRepository, times(1)).save(argThat(saveUser ->
+                saveUser.getEmail().equals(dto.email())
+                && saveUser.getPassword() != null
+                && !saveUser.getPassword().equals(dto.password())
+                ));
+
+
+    }
+
+    @Test
+    @DisplayName("Should return 400 when email already exists")
+    void register_ExistingEmail_ReturnsBadRequest() throws Exception{
+        RegisterDTO dto = new RegisterDTO("existing@user.com","anyPass",Set.of(Role.USER));
+
+        when(userRepository.findByEmail(dto.email())).thenReturn(new User("existing@user.com","encrypted", Set.of(Role.USER)));
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(String.valueOf(MediaType.APPLICATION_JSON))
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isBadRequest());
+
+        verify(userRepository, never()).save(any());
     }
 }
